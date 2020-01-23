3c0cccd06990c0e21e71c70893b66490354c28ff
Cleiton Maciel <cleitonmaciel@gestaosistemas.com>
2020-01-23 18:32:27 -0300

    adicionando os fontes

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-qe3yu.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+
+//GET, POST, PUT, DELETE
+
+// req.query = acessar query params
+// req.params = acessar router params (para edicao, delete)
+// req.body = acessar corpo da requisicao (para criacao, edicao)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
